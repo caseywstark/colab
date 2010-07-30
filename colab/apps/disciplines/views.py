ef74019786c1a135fdc151a52d23d6859a77f4ba
Brian Rosner <brosner@gmail.com>
2010-07-29 19:54:36 -0600

    fixed missing import

--- colab/apps/disciplines/views.py
+++ colab/apps/disciplines/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
