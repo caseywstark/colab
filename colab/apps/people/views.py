70f67c6a0d3b19869c043126056c51fc80feabf3
Brian Rosner <brosner@gmail.com>
2010-07-30 10:51:14 -0600

    added some missing imports

--- colab/apps/people/views.py
+++ colab/apps/people/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
 from django.utils.translation import ugettext
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
