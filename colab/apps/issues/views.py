d3ffab73236a2aac05aef24a86379ed390f1415d
Casey Stark <casey@thestarkeffect.com>
2010-08-03 02:14:47 -0700

    Fixed my issues filtering

--- colab/apps/issues/views.py
+++ colab/apps/issues/views.py
@@ -100,7 +100,7 @@ def issues(request, mine=False, template_name="issues/issues.html"):
     authenticated = request.user.is_authenticated()
     
     if authenticated and mine:
-        issues = Issue.objects.filter(contributors=request.user)
+        issues = Issue.objects.filter(contributor_users=request.user)
     else:
         issues = Issue.objects.filter(private=False)
     
