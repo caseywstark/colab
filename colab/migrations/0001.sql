c3931de014c57412632b0c5ccb35bcb065255e69
Casey Stark <casey@thestarkeffect.com>
2010-08-03 23:16:49 -0700

    Working on postgresql now. Corrected default boolean values to 'f' to fit psql

--- colab/migrations/0001.sql
+++ colab/migrations/0001.sql
@@ -1,2 +1,2 @@
-ALTER TABLE issues_issue ADD COLUMN sandbox boolean NOT NULL default 0;
-ALTER TABLE issues_issue ADD COLUMN model_project boolean NOT NULL default 0;
+ALTER TABLE issues_issue ADD COLUMN sandbox boolean NOT NULL default 'f';
+ALTER TABLE issues_issue ADD COLUMN model_project boolean NOT NULL default 'f';
--- colab/settings.py
+++ colab/settings.py
@@ -29,10 +29,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "NAME": "dev.db",                       # Or path to database file if using sqlite3.
-        "USER": "dev.db",                             # Not used with sqlite3.
-        "PASSWORD": "",                         # Not used with sqlite3.
+        "ENGINE": "django.db.backends.postgresql_psycopg2", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
+        "NAME": "dev",                       # Or path to database file if using sqlite3.
+        "USER": "dev",                       # Not used with sqlite3.
+        "PASSWORD": "password",                     # Not used with sqlite3.
         "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
         "PORT": "",                             # Set to empty string for default. Not used with sqlite3.
     }
